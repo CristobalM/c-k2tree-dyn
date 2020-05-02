343b203f305aa862791a769fff886307856f88c7
Cristobal Miranda <cristobal.miranda.tt@gmail.com>
2020-05-02 14:33:00 -0400

    avoid inlining memory functions

--- src/default_memalloc.c
+++ src/default_memalloc.c
@@ -11,58 +11,58 @@
 
 #include "definitions.h"
 
-struct block *k2tree_alloc_block(void) {
+struct block * __attribute__ ((noinline)) k2tree_alloc_block(void) {
   return (struct block *)calloc(1, sizeof(struct block));
 }
 
-struct block_topology *k2tree_alloc_block_topology(void) {
+struct block_topology * __attribute__ ((noinline)) k2tree_alloc_block_topology(void) {
   return (struct block_topology *)calloc(1, sizeof(struct block_topology));
 }
 
-struct block_frontier *k2tree_alloc_block_frontier(void) {
+struct block_frontier * __attribute__ ((noinline)) k2tree_alloc_block_frontier(void) {
   return (struct block_frontier *)calloc(1, sizeof(struct block_frontier));
 }
 
-struct bitvector *k2tree_alloc_bitvector(void) {
+struct bitvector * __attribute__ ((noinline)) k2tree_alloc_bitvector(void) {
   return (struct bitvector *)calloc(1, sizeof(struct bitvector));
 }
 
-struct vector *k2tree_alloc_vector(void) {
+struct vector * __attribute__ ((noinline)) k2tree_alloc_vector(void) {
   return (struct vector *)calloc(1, sizeof(struct vector));
 }
 
-struct u32array_alloc k2tree_alloc_u32array(int size) {
+struct u32array_alloc __attribute__ ((noinline)) k2tree_alloc_u32array(int size) {
   struct u32array_alloc out;
   out.data = (uint32_t *)calloc(size, sizeof(uint32_t));
   out.size = size;
   return out;
 }
 
-int k2tree_free_block(struct block *b) {
+int __attribute__ ((noinline)) k2tree_free_block(struct block *b) {
   free(b);
   return SUCCESS_ECODE;
 }
 
-int k2tree_free_block_topology(struct block_topology *bt) {
+int __attribute__ ((noinline)) k2tree_free_block_topology(struct block_topology *bt) {
   free(bt);
   return SUCCESS_ECODE;
 }
 
-int k2tree_free_block_frontier(struct block_frontier *bf) {
+int __attribute__ ((noinline)) k2tree_free_block_frontier(struct block_frontier *bf) {
   free(bf);
   return SUCCESS_ECODE;
 }
 
-int k2tree_free_bitvector(struct bitvector *bv) {
+int __attribute__ ((noinline)) k2tree_free_bitvector(struct bitvector *bv) {
   free(bv);
   return SUCCESS_ECODE;
 }
-int k2tree_free_vector(struct vector *v) {
+int __attribute__ ((noinline)) k2tree_free_vector(struct vector *v) {
   free(v);
   return SUCCESS_ECODE;
 }
 
-int k2tree_free_u32array(struct u32array_alloc alloc) {
+int __attribute__ ((noinline)) k2tree_free_u32array(struct u32array_alloc alloc) {
   free(alloc.data);
   return SUCCESS_ECODE;
 }
