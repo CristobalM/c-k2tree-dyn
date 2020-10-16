e23e93866cb6487b23f296444e42f07d0216d4e3
CristobalM <cristobal.miranda.tt@gmail.com>
2020-10-16 20:47:36 -0300

    benchmarks

--- benchmarks/insertions_benchmarks.cpp
+++ benchmarks/insertions_benchmarks.cpp
@@ -36,15 +36,25 @@ extern "C" {
 std::mt19937 rng(123321);
 
 void random_benchmark_by_depth(uint32_t treedepth, uint32_t points_count);
+void random_benchmark_by_depth_dense(uint32_t treedepth, uint32_t points_count);
 std::vector<unsigned long> create_shuffled_sequence(unsigned long sequence_size,
                                                     unsigned long space_size);
 
 int main(void) {
 
+  random_benchmark_by_depth(30, 1 << 25 );
+
+  for(unsigned long i = 2; i < 12; i++){
+    random_benchmark_by_depth_dense(i, 1 << i);
+  }
+
+  for(unsigned long i = 2; i < 20; i++){
+    random_benchmark_by_depth(i, 1 << i);
+  }
   random_benchmark_by_depth(24, 1 << 20);
   random_benchmark_by_depth(28, 1 << 20);
   random_benchmark_by_depth(30, 1 << 20);
-  random_benchmark_by_depth(32, 1 << 20);
+  
 
   return 0;
 }
@@ -113,6 +123,86 @@ void random_benchmark_by_depth(uint32_t treedepth, uint32_t points_count) {
       std::cerr << "Point " << i << " not found (" << random_seq_1[i] << ", "
                 << random_seq_2[i] << ")" << std::endl;
       exit(1);
+    }
+  }
+  stop = std::chrono::high_resolution_clock::now();
+
+  duration =
+      std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+  std::cout << "\n\nSuccesful Querying Results\n";
+  std::cout << "Total Time in Microseconds: " << duration.count() << std::endl;
+  std::cout << "Average Time querying one point in Microseconds: "
+            << duration.count() / points_count << std::endl;
+#ifdef DEBUG_STATS
+  std::cout << "Time spent on sequential scan in total "
+            << qs.dstats.time_on_sequential_scan << " microseconds"
+            << std::endl;
+  std::cout << "Time spent on sequential scan in average "
+            << qs.dstats.time_on_sequential_scan / points_count
+            << " microseconds" << std::endl;
+  qs.dstats.time_on_sequential_scan = 0;
+#endif
+
+  std::cout << "-------------------\n\n\n" << std::endl;
+
+  free_rec_block(root_block);
+  finish_queries_state(&qs);
+}
+
+void random_benchmark_by_depth_dense(uint32_t treedepth, uint32_t points_count) {
+  unsigned long side = 1 << treedepth;
+  struct block *root_block = create_block(static_cast<TREE_DEPTH_T>(treedepth));
+
+  struct queries_state qs;
+  init_queries_state(&qs, treedepth);
+
+  auto random_seq_1 = create_shuffled_sequence(points_count, side);
+  auto random_seq_2 = create_shuffled_sequence(points_count, side);
+
+  std::cout << "-------------------\n";
+  std::cout << "Started random_benchmark_by_depth_dense with treedepth = "
+            << treedepth << " and points_count = " << points_count << std::endl;
+  auto start = std::chrono::high_resolution_clock::now();
+
+  for (size_t i = 0; i < points_count; i++) {
+    for(size_t j = 0; j < points_count; j++){
+     insert_point(root_block, random_seq_1[i], random_seq_2[j], &qs);
+    }
+  }
+
+  auto stop = std::chrono::high_resolution_clock::now();
+
+  auto duration =
+      std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+  std::cout << "\n\nInsertion results\n";
+  std::cout << "Total Time in Microseconds: " << duration.count() << std::endl;
+  std::cout << "Average Time inserting one point in Microseconds: "
+            << duration.count() / points_count << std::endl;
+
+#ifdef DEBUG_STATS
+  std::cout << "Time spent on sequential scan in total "
+            << qs.dstats.time_on_sequential_scan << " microseconds"
+            << std::endl;
+  std::cout << "Time spent on sequential scan in average "
+            << qs.dstats.time_on_sequential_scan / points_count
+            << " microseconds" << std::endl;
+  qs.dstats.time_on_sequential_scan = 0;
+#endif
+
+  start = std::chrono::high_resolution_clock::now();
+
+  for (size_t i = 0; i < points_count; i++) {
+        for(size_t j = 0; j < points_count; j++){
+    int has_point_result;
+    has_point(root_block, random_seq_1[i], random_seq_2[j], &qs,
+              &has_point_result);
+    if (!has_point_result) {
+      std::cerr << "Point " << i << " not found (" << random_seq_1[i] << ", "
+                << random_seq_2[j] << ")" << std::endl;
+      exit(1);
+    }
     }
   }
   stop = std::chrono::high_resolution_clock::now();
