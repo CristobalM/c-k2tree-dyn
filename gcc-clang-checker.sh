4643a19cd29ee72063a6d40d14bad77a5d106087
CristobalM <cristobal.miranda.tt@gmail.com>
2020-10-26 10:11:42 -0300

    update gcc lang checker

--- gcc-clang-checker.sh
+++ gcc-clang-checker.sh
@@ -3,21 +3,30 @@
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
 
-make re > /dev/null 2> /dev/null
-
+mkdir -p _tmp_build_0
+cd _tmp_build_0
+cmake .. > /dev/null 2> /dev/null
+make -j4 > /dev/null 2> /dev/null
 if [ $? -eq 0 ]; then
     echo "Works with GNU Compiler"
 else
     echo "Doesn't work with GNU Compiler"
 fi
+cd ..
+rm -rf _tmp_build_0
+
 
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 
-make re > /dev/null 2> /dev/null 
-
+mkdir -p _tmp_build_0
+cd _tmp_build_0
+cmake .. > /dev/null 2> /dev/null
+make -j4 > /dev/null 2> /dev/null
 if [ $? -eq 0 ]; then
     echo "Works with CLANG Compiler";
 else
     echo "Doesn't work with CLANG Compiler";
 fi
+cd ..
+rm -rf _tmp_build_0
